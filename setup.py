382eab2c142a4310f91b1f7b277e770f1cba59be
Paul <runette@gmail.com>
2021-11-26 13:19:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -72,10 +72,9 @@ setup_args = dict(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: GIS',
     ],
 
